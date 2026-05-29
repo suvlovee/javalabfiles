3d4be04195722a2e5da9b39a9e934129b5aea21a
Suvlovee <suvloveshah@gmail.com>
2026-05-29 10:38:46 +0545

    reached home and done some practice

---
+++ practiceclass/ExceptionDemo.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class ExceptionDemo{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter two integers (a and b): ");
+        
+        try {
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+            int c = a / b; // This will throw ArithmeticException
+            System.out.println("Result: " + c);
+        } catch (ArithmeticException ae) {
+            System.out.println("Cannot divide by zero. Please provide a non-zero divisor."+ ae.getMessage());
+        } catch (Exception e){
+            System.out.println("Please enter valid integers. " + e.getMessage());
+        }
+    }
+}
